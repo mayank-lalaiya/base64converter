86eb16de51e60895b75c755e4488cf8c35411d88
mayank-lalaiya <83688098+mayank-lalaiya@users.noreply.github.com>
2024-08-26 17:44:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set view engine
+app.set('view engine', 'ejs');
+
+// Routes
+const decodeRoute = require('./routes/decode');
+app.use('/decode', decodeRoute);
+
+// Home route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
